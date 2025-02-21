fea188e29737e0b65d582c010a6523ede8b49a35
Aashish Singh <11aashish.singh@gmail.com>
2025-02-21 15:51:42 +0530

    fixed spline related error

--- src/components/sections/hero/hero.jsx
+++ src/components/sections/hero/hero.jsx
@@ -1,14 +1,12 @@
-"use client";
-import TextPressure from "@/blocks/TextAnimations/TextPressure/TextPressure";
-import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
-import Coins from "@/components/spline/coins";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Image from "next/image";
-import Link from "next/link";
-import { useLayoutEffect, useRef } from "react";
-
-// Register GSAP ScrollTrigger
+'use client';
+import TextPressure from '@/blocks/TextAnimations/TextPressure/TextPressure';
+import { InteractiveHoverButton } from '@/components/magicui/interactive-hover-button';
+import Spline from '@splinetool/react-spline';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useLayoutEffect, useRef } from 'react';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
@@ -34,8 +32,8 @@ export default function Hero() {
                         y,
                         scrollTrigger: {
                             trigger: ref.current,
-                            start: "top bottom",
-                            end: "bottom top",
+                            start: 'top bottom',
+                            end: 'bottom top',
                             scrub: true,
                         },
                     }
@@ -43,89 +41,105 @@ export default function Hero() {
             }
         });
 
-        return () => ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        return () =>
+            ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     }, []);
 
     return (
-        <section className="flex flex-col relative items-center justify-center w-full px-6 py-12 md:py-20 lg:py-24 xl:py-28 mb-24 md:mb-36">
+        <section className='flex flex-col relative items-center justify-center w-full px-6 py-12 md:py-20 lg:py-24 xl:py-28 mb-24 md:mb-36'>
             {/* Text Content */}
-            <div className="max-w-4xl relative text-center flex flex-col items-center gap-6">
-                <h1 className="text-3xl sm:text-5xl font-bold uppercase leading-tight">
-                    <div className="flex flex-wrap items-center justify-center gap-3 md:gap-4">
+            <div className='max-w-4xl relative text-center flex flex-col items-center gap-6'>
+                <h1 className='text-3xl sm:text-5xl font-bold uppercase leading-tight'>
+                    <div className='flex flex-wrap items-center justify-center gap-3 md:gap-4'>
                         <span>Fueling your growth</span>
-                        <span className="relative w-10 h-10 md:w-14 md:h-14 -rotate-12 hidden md:inline-block">
+                        <span className='relative w-10 h-10 md:w-14 md:h-14 -rotate-12 hidden md:inline-block'>
                             <Image
-                                src="/images/hero-images/image.png"
-                                alt="Vivid Motion Logo"
+                                src='/images/hero-images/image.png'
+                                alt='Vivid Motion Logo'
                                 width={56}
                                 height={56}
-                                className="object-contain animate-float"
+                                className='object-contain animate-float'
                                 priority
                             />
                         </span>
                     </div>
                     <span>in web3 solutions with</span>
                     <TextPressure
-                        text="Limitless ✦ Creativity"
-                        className="mt-2 md:mt-4"
-                        fontFamily="inter"
-                        textColor="#D9B343"
+                        text='Limitless ✦ Creativity'
+                        className='mt-2 md:mt-4'
+                        fontFamily='inter'
+                        textColor='#D9B343'
                         minFontSize={5}
                     />
                 </h1>
-                <Link href="#" className="group transition-transform hover:scale-105 active:scale-95">
-                    <InteractiveHoverButton className="px-6 py-3 sm:px-8 sm:py-4 text-base sm:text-lg">
+                <Link
+                    href='#'
+                    className='group transition-transform hover:scale-105 active:scale-95'
+                >
+                    <InteractiveHoverButton className='px-6 py-3 sm:px-8 sm:py-4 text-base sm:text-lg'>
                         Book a call
                     </InteractiveHoverButton>
                 </Link>
             </div>
 
             {/* 3D Visualization with Decorative Rocks */}
-            <div className="w-full relative max-w-4xl h-[60vh] sm:h-[65vh] md:h-[75vh] max-h-[700px] mt-12 md:mt-16">
-                <Coins />
+            <div className='w-full relative max-w-4xl h-[60vh] sm:h-[65vh] md:h-[75vh] max-h-[700px] mt-12 md:mt-16'>
+                <Spline
+                    scene='https://prod.spline.design/Tq4nAM0x1iaR7TS3/scene.splinecode'
+                    className='w-full h-full rounded-xl'
+                />
 
                 {/* Rock 3 */}
-                <div className="absolute z-10 -bottom-6 right-0 w-[20%] max-w-[150px] sm:max-w-[180px]">
+                <div className='absolute z-10 -bottom-6 right-0 w-[20%] max-w-[150px] sm:max-w-[180px]'>
                     <Image
-                        src="/images/bg/rock3.svg"
-                        alt="Decorative rock 3"
+                        src='/images/bg/rock3.svg'
+                        alt='Decorative rock 3'
                         width={150}
                         height={150}
-                        className="object-contain"
+                        className='object-contain'
                     />
                 </div>
 
                 {/* Rock 1 */}
-                <div ref={rock1Ref} className="absolute z-10 top-0 left-0 w-[18%] max-w-[140px] sm:max-w-[160px]">
+                <div
+                    ref={rock1Ref}
+                    className='absolute z-10 top-0 left-0 w-[18%] max-w-[140px] sm:max-w-[160px]'
+                >
                     <Image
-                        src="/images/bg/rock1.svg"
-                        alt="Decorative rock 1"
+                        src='/images/bg/rock1.svg'
+                        alt='Decorative rock 1'
                         width={160}
                         height={160}
-                        className="object-contain"
+                        className='object-contain'
                     />
                 </div>
             </div>
 
             {/* Additional Rock Decorations */}
-            <div ref={rock1Ref} className="absolute left-24 top-36 -z-10 w-[22%] max-w-[180px] sm:max-w-[200px]">
+            <div
+                ref={rock1Ref}
+                className='absolute left-24 top-36 -z-10 w-[22%] max-w-[180px] sm:max-w-[200px]'
+            >
                 <Image
-                    src="/images/bg/rock1.svg"
-                    alt="Decorative rock 1"
+                    src='/images/bg/rock1.svg'
+                    alt='Decorative rock 1'
                     width={200}
                     height={200}
-                    className="object-contain"
+                    className='object-contain'
                 />
             </div>
 
             {/* Rock 2 */}
-            <div ref={rock2Ref} className="absolute right-24 top-48 z-10 w-[24%] max-w-[190px] sm:max-w-[220px]">
+            <div
+                ref={rock2Ref}
+                className='absolute right-24 top-48 z-10 w-[24%] max-w-[190px] sm:max-w-[220px]'
+            >
                 <Image
-                    src="/images/bg/rock2.svg"
-                    alt="Decorative rock 2"
+                    src='/images/bg/rock2.svg'
+                    alt='Decorative rock 2'
                     width={220}
                     height={220}
-                    className="object-contain"
+                    className='object-contain'
                 />
             </div>
         </section>
