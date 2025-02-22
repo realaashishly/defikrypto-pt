cdf28b74bb18dcb0d2de86a3d46d82d4a440324a
Aashish Singh <11aashish.singh@gmail.com>
2025-02-22 12:04:03 +0530

    added vercel speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         >
 
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
